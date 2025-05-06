f967fbed491e485b93fa95c56adaf9ad18275df0
olf4rht <153188624+olf4rht@users.noreply.github.com>
2025-05-06 12:03:40 +0200

    Delete nextjs-app directory

--- nextjs-app/app/[locale]/loading.js
+++
@@ -1,7 +0,0 @@
-export default function loading() {
-  return (
-    <div className="min-h-screen pt-60">
-      <div className="spinner"></div>
-    </div>
-  );
-}
--- nextjs-app/lib/mapSlugsWithLocales.js
+++
@@ -1,8 +0,0 @@
-function mapSlugsWithLocales(translations, { currentLocalization }) {
-  return translations.reduce((acc, translation) => {
-    acc[translation.language] = translation.slug.current; // Map locale to its slug
-    return acc;
-  }, currentLocalization);
-}
-
-export default mapSlugsWithLocales;
